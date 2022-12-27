41998e518e87f2493578aa2474afb4e99096b3f2
Ariel <narivas@gylgroup.com>
2022-12-26 22:27:05 -0300

    nuevo ejer

---
+++ EscibirLeerDeUnArchivo/EscibirLeerDeUnArchivo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using static System.Console; 
+
+namespace EscibirLeerDeUnArchivo
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {   
+            //Append
+            //Create
+            //CreateNew
+            //Open
+            //OpenOrCreate
+            //Truncate
+            FileStream fsEscribir = new FileStream("MiArchivoDos.txt", FileMode.Append);
+
+            string cadena = "Estoy escribiendo una cadena y es una cadena añadida";
+            fsEscribir.Write(ASCIIEncoding.ASCII.GetBytes(cadena), 0, cadena.Length);
+            fsEscribir.Close();
+
+            byte[] infoArchivo = new byte[100]; 
+
+            FileStream fs = new FileStream("MiArchivoDos.txt", FileMode.Open);
+            fs.Read(infoArchivo, 0, (int)fs.Length);
+            WriteLine(ASCIIEncoding.ASCII.GetString(infoArchivo));
+            ReadKey();
+            fs.Close();
+
+
+        }
+    }
+}
+
